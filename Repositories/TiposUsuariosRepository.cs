881ad5f853d861362fad8e5e1f4d08490fe18605
HIORHANNA COUTO SANTANA <52267963825@info.local>
2025-03-18 16:46:46 -0300

    atualizar

---
+++ Repositories/TiposUsuariosRepository.cs
@@ -0,0 +1,20 @@
+﻿using EventPlus_.Context;
+
+namespace EventPlus_.Repositories
+{
+    public class TiposUsuariosRepository
+    {
+        private readonly Eventos_Context? _Context;
+
+        public TiposUsuariosRepository(Eventos_Context? context) 
+        {
+            _Context = context;
+        }
+
+        public void Atualizar(Guid id, TiposUsuariosRepository tiposUsuarios)
+        {
+
+        }
+
+    }
+}
